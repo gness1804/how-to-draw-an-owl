85dc3ce59e27dd178d4204c6f7dcd988ac26c8bb
gness1804 <graham.nessler@gmail.com>
2016-11-29 12:58:28 -0700

    finished bottom part of body

--- canvas.js
+++ canvas.js
@@ -1,3 +1,14 @@
 //the boilerplate canvas setup
 const canvas = document.getElementById("canvas");
 const draw = canvas.getContext("2d");
+
+//the bottom part of the body
+
+draw.beginPath();
+draw.arc(200, 400, 100, 0, 2*Math.PI);
+// draw.strokeStyle = ""; //not needed if filling the entire thing
+draw.fillStyle = "rgb(152, 145, 158)";
+draw.fill();
+// draw.closePath(); //not needed if using "fill()"
+
+//the top part of the body
