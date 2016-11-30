3e779e7a204055efe3956fc5f24a845e239d2e5a
gness1804 <graham.nessler@gmail.com>
2016-11-29 20:48:17 -0700

    finished w bottom part of body

--- canvas.js
+++ canvas.js
@@ -5,10 +5,8 @@ const draw = canvas.getContext("2d");
 //the bottom part of the body
 
 draw.beginPath();
-draw.arc(200, 400, 100, 0, 2*Math.PI);
-// draw.strokeStyle = ""; //not needed if filling the entire circle
-draw.fillStyle = "rgb(152, 145, 158)";
+draw.arc(354, 519, 150, 0, 2*Math.PI);
+// draw.strokeStyle = "//color or style"; //not needed if drawing a filled circle
+draw.fillStyle = "rgb(175, 184, 190)";
 draw.fill();
 // draw.closePath(); //not needed if using "fill()"
-
-//the top part of the body
